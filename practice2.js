27925f2a7459c38074655059688a509ca8fc6d3a
JonasZHGIT <zhongjonas@gmail.com>
2018-01-21 15:49:55 +0800

    test ip

--- practice2.js
+++ practice2.js
@@ -3,6 +3,6 @@ var os = require("os");
 var interface = os.networkInterfaces();
 http.createServer(function(req, res) {
 	// var user_ip = req.connection.remoteAddress;
-	console.log(interface.lo0[0].address);
+	console.log(interface.lo[0].address);
 	res.end();
 }).listen(process.env.PORT || 8000);
