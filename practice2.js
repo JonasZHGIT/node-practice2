3ef36d5712e6dbba65226db8cad9472c23d84c9e
JonasZHGIT <zhongjonas@gmail.com>
2018-01-21 16:18:17 +0800

    test ip

--- practice2.js
+++ practice2.js
@@ -2,7 +2,7 @@ var http = require("http");
 var os = require("os");
 var interface = os.networkInterfaces();
 http.createServer(function(req, res) {
-	var forwardedIp = req.headers['x-real-ip'] || req.header['x-forwarded-for'];
+	var forwardedIp = req.headers['x-real-ip'] || req.headers['x-forwarded-for'];
 	var user_ip = forwardedIp?forwardedIp:req.connection.remoteAddress;
 	var result = {
 		"ipaddress": user_ip
